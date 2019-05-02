83c4387b707441643e8c554e335821203e4daa30
Ricardo Mendes <ricardomlealjr@gmail.com>
2019-05-02 11:58:24 -0300

    Upload of image using multer and inserting in the database using sequelize

--- node/challenge-2/src/routes.js
+++ node/challenge-2/src/routes.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const multerConfig = require('./config/multer')
+const upload = require('multer')(multerConfig)
 
 const routes = express.Router()
+const UserController = require('./app/controllers/UserController')
 
-routes.get('/', (req, res) => {
-    return res.render('auth/signup')
-})
+routes.get('/signup', UserController.create)
+routes.post('/signup', upload.single('avatar'), UserController.store)
 
 module.exports = routes
