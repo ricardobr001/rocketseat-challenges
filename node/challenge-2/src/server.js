b08d7bf6d6ea92144f784932bed9d6292063fc30
Ricardo Mendes <ricardomlealjr@gmail.com>
2019-05-02 10:24:03 -0300

    Using the migration to create table in the DB

--- node/challenge-2/src/server.js
+++ node/challenge-2/src/server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
-const path  =require('path')
+const path = require('path')
 
 class App {
-    constructor() {
+    constructor () {
         this.express = express()
         this.isDev = process.env.NODE_ENV !== 'production'
 
@@ -12,21 +12,22 @@ class App {
         this.routes()
     }
 
-    middlewares() {
+    middlewares () {
         this.express.use(express.urlencoded({ extended: false }))
     }
 
-    views() {
+    views () {
         nunjucks.configure(path.resolve(__dirname, 'app', 'views'), {
             watch: this.isDev,
             express: this.express,
             autoescape: true
         })
 
+        this.express.use(express.static(path.resolve(__dirname, 'public')))
         this.express.set('view engine', 'njk')
     }
 
-    routes() {
+    routes () {
         this.express.use(require('./routes'))
     }
 }
