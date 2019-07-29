6d0d1efe27be8fb5d1571db5beffc28d6c3c7f29
Ricardo Mendes <ricardomlealjr@gmail.com>
2019-07-28 21:02:53 -0300

    Email working

--- node/challenge-3/src/app/Services/Mail.js
+++ node/challenge-3/src/app/Services/Mail.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer')
 const path = require('path')
 const hbs = require('nodemailer-express-handlebars')
-const exphbs = require('express-handlebars')
+// const exphbs = require('express-handlebars')
 const mailConfig = require('../../config/mail')
 
 const transport = nodemailer.createTransport(mailConfig)
@@ -11,9 +11,11 @@ const viewPath = path.resolve(__dirname, '..', 'views', 'emails')
 transport.use(
     'compile',
     hbs({
-        viewEngine: exphbs.create({
-            partialsDir: path.resolve(viewPath, 'partials')
-        }),
+        viewEngine: {
+            extName: '.hbs',
+            partialsDir: `${viewPath}/partials`,
+            layoutsDir: `${viewPath}/layouts`
+        },
         viewPath: viewPath,
         extName: '.hbs'
     })
