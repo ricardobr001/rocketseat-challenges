b8d2803de5da10ec5958f03643e9b8dbfeb3e605
Ricardo Mendes <ricardomlealjr@gmail.com>
2019-08-12 08:25:30 -0300

    Configuring SMTP mail on adonis

--- node/challenge-4/app/Controllers/Http/ForgotPasswordController.js
+++ node/challenge-4/app/Controllers/Http/ForgotPasswordController.js
@@ -2,6 +2,7 @@
 
 const crypto = require('crypto')
 const User = use('App/Models/User')
+const Mail = use('Mail')
 
 class ForgotPasswordController {
   async store ({ request, response }) {
@@ -13,6 +14,20 @@ class ForgotPasswordController {
       user.token_created_at = new Date()
 
       await user.save()
+      await Mail.send(
+        ['emails.forgot_password'],
+        {
+          email,
+          token: user.token,
+          link: `${request.input('redirect_url')}?token=${user.token}`
+        },
+        message => {
+          message
+            .to(user.email)
+            .from('adonis-challenge@email.com', 'Adonis Challenge')
+            .subject('Recovering password')
+        }
+      )
     } catch (err) {
       return response.status(err.status).send({
         error: { message: 'something went wrong, this e-mail exists?' }
