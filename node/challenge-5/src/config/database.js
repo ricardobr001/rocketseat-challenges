0a8c908d2c6f66355c2271683f35da14c32246e6
Ricardo Mendes <ricardomlealjr@gmail.com>
2019-08-19 23:45:32 -0300

    eslint configured for jest and passing the test from /sessions

--- node/challenge-5/src/config/database.js
+++ node/challenge-5/src/config/database.js
@@ -8,6 +8,7 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT || 'postgres',
+    logging: false,
     storage: './__tests__/database.sqlite',
     operatorsAliases: false,
     define: {
