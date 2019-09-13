b92d4a9359ed488f032ff2bed10e11d54b9e5bdc
Ricardo Mendes <ricardomlealjr@gmail.com>
2019-09-13 19:12:29 -0300

    Showing the repositories, saving in the localStorage and routing to the repository component

--- react/challenge-1/src/pages/Main/index.jsx
+++ react/challenge-1/src/pages/Main/index.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa'
 
 import api from '../../services/api'
 
-import { Container, Form, SubmitButton } from './styles'
+import { Container, Form, SubmitButton, List } from './styles'
 
 export default class Main extends Component {
   constructor(props) {
@@ -12,6 +13,22 @@ export default class Main extends Component {
       newRepo: '',
       repositories: [],
       loading: false
+    }
+  }
+
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories')
+
+    if (repositories) {
+      this.setState({ repositories: JSON.parse(repositories) })
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state
+
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories))
     }
   }
 
@@ -37,7 +54,7 @@ export default class Main extends Component {
   }
 
   render() {
-    const { newRepo, loading } = this.state
+    const { newRepo, repositories, loading } = this.state
 
     return (
       <Container>
@@ -62,6 +79,17 @@ export default class Main extends Component {
             )}
           </SubmitButton>
         </Form>
+
+        <List>
+          {repositories.map(repository => (
+            <li key={repository.name}>
+              <span>{repository.name}</span>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Details
+              </Link>
+            </li>
+          ))}
+        </List>
       </Container>
     )
   }
