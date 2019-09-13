9a015a1c91e99ee071840bf9a7cbe4d9411ad3c9
Ricardo Mendes <ricardomlealjr@gmail.com>
2019-09-13 08:25:29 -0300

    Styles based on the component state and calling the github API

--- react/challenge-1/src/pages/Main/index.jsx
+++ react/challenge-1/src/pages/Main/index.jsx
@@ -1,25 +1,68 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa'
 
-import { FaGithub, FaPlus } from 'react-icons/fa'
+import api from '../../services/api'
+
 import { Container, Form, SubmitButton } from './styles'
 
-function Main() {
-  return (
-    <Container>
-      <h1>
-        <FaGithub />
-        Repositories
-      </h1>
+export default class Main extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      newRepo: '',
+      repositories: [],
+      loading: false
+    }
+  }
 
-      <Form>
-        <input type="text" placeholder="Add repository" />
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value })
+  }
 
-        <SubmitButton disabled>
-          <FaPlus color="#FFFFFF" size={14} />
-        </SubmitButton>
-      </Form>
-    </Container>
-  )
+  handleSubmit = async e => {
+    e.preventDefault()
+    this.setState({ loading: true })
+    const { newRepo, repositories } = this.state
+
+    const res = await api.get(`/repos/${newRepo}`)
+    const data = {
+      name: res.data.full_name
+    }
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: false
+    })
+  }
+
+  render() {
+    const { newRepo, loading } = this.state
+
+    return (
+      <Container>
+        <h1>
+          <FaGithub />
+          Repositories
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Add repository"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#FFFFFF" size={14} />
+            ) : (
+              <FaPlus color="#FFFFFF" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+      </Container>
+    )
+  }
 }
-
-export default Main
