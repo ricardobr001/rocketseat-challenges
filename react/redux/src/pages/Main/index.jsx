4b02f982d3352cf1c81dd6a9f021df279edebb6e
Ricardo Mendes <ricardomlealjr@gmail.com>
2019-09-15 14:06:08 -0300

    Consuming the mock

--- react/redux/src/pages/Main/index.jsx
+++ react/redux/src/pages/Main/index.jsx
@@ -1,27 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import { MdAddShoppingCart } from 'react-icons/md'
+import api from '../../services/api'
+import { formatPrice } from '../../util/format'
+
 import { ProductList } from './styles'
 
-import sneaker from '../../assets/images/sneaker.jpg'
+export default class Main extends Component {
+  constructor() {
+    super()
+    this.state = {
+      products: []
+    }
+  }
 
-function Main() {
-  return (
-    <ProductList>
-      <li>
-        <img src={sneaker} alt="sneaker" />
-        <strong>Sneaker description</strong>
-        <span>U$ 40,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
+  async componentDidMount() {
+    const res = await api.get('/products')
 
-          <span>ADD TO CART</span>
-        </button>
-      </li>
-    </ProductList>
-  )
+    const data = res.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price)
+    }))
+
+    this.setState({ products: data })
+  }
+
+  render() {
+    const { products } = this.state
+
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.id} />
+            <strong>{product.title}</strong>
+            <span>U$ {product.priceFormatted}</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" /> 3
+              </div>
+
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    )
+  }
 }
-
-export default Main
