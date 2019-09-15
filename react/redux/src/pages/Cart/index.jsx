ea9cfd645f9b70eab8f10853a70532702a1a2856
Ricardo Mendes <ricardomlealjr@gmail.com>
2019-09-15 12:06:05 -0300

    Component cart stylized

--- react/redux/src/pages/Cart/index.jsx
+++ react/redux/src/pages/Cart/index.jsx
@@ -1,7 +1,66 @@
 import React from 'react'
 
-// import { Container } from './styles';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete
+} from 'react-icons/md'
+import { Container, ProductTable, Total } from './styles'
+
+import sneaker from '../../assets/images/sneaker.jpg'
 
 export default function Cart() {
-  return <div />
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUCT</th>
+            <th>QUANTITY</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img src={sneaker} alt="sneaker" />
+            </td>
+            <td>
+              <strong>Sneaker title</strong>
+              <span>U$ 40,00</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>U$ 80,00</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Finalize order</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>U$ 80,00</strong>
+        </Total>
+      </footer>
+    </Container>
+  )
 }
