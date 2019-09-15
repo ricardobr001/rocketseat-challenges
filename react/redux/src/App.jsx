de54db1fef199afb0276ad1e72c0b85c875ef467
Ricardo Mendes <ricardomlealjr@gmail.com>
2019-09-15 11:28:04 -0300

    Showing the sneaker avaible to buy on Main component

--- react/redux/src/App.jsx
+++ react/redux/src/App.jsx
@@ -9,8 +9,8 @@ import Header from './components/Header'
 function App() {
   return (
     <>
-      <GlobalStyle />
       <BrowserRouter>
+        <GlobalStyle />
         <Header />
         <Routes />
       </BrowserRouter>
--- react/redux/src/pages/Main/index.jsx
+++ react/redux/src/pages/Main/index.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
 
+import { MdAddShoppingCart } from 'react-icons/md'
+import { ProductList } from './styles'
+
+import sneaker from '../../assets/images/sneaker.jpg'
+
 function Main() {
-  return <h1>Hello World</h1>
+  return (
+    <ProductList>
+      <li>
+        <img src={sneaker} alt="sneaker" />
+        <strong>Sneaker description</strong>
+        <span>U$ 40,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+    </ProductList>
+  )
 }
 
 export default Main
