b257bb3c3340e588a39e21682067f170432f6ac9
Ricardo Mendes <ricardomlealjr@gmail.com>
2019-09-17 19:58:43 -0300

    Refactoring the use of redux-devtools

--- react/redux/src/store/index.js
+++ react/redux/src/store/index.js
@@ -7,15 +7,17 @@ import rootSaga from './modules/rootSaga'
 const sagaMiddleware = createSagaMiddleware()
 
 /* eslint-disable no-underscore-dangle */
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(sagaMiddleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-)
+const enchancer =
+  process.env.NODE_ENV === 'development'
+    ? compose(
+        applyMiddleware(sagaMiddleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      )
+    : null
 /* eslint-enable */
 
+const store = createStore(rootReducer, enchancer)
 sagaMiddleware.run(rootSaga)
 
 export default store
