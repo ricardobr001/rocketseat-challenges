2e444c1f928782a8fb3386f9f3adca1b07181187
Ricardo Mendes <ricardomlealjr@gmail.com>
2019-09-23 07:40:41 -0300

    Testing if the component is calling the reducer with a specific object type and value

--- react/tests/src/__tests__/components/TechList.test.js
+++ react/tests/src/__tests__/components/TechList.test.js
@@ -1,20 +1,35 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-import { render } from '@testing-library/react'
+import { useSelector, useDispatch } from 'react-redux'
+import { render, fireEvent } from '@testing-library/react'
 
+import { addTech } from '../../store/modules/techs/action'
 import TechList from '../../components/TechList'
 
 jest.mock('react-redux')
 
 describe('TechList component', () => {
   it('should render tech list', () => {
-    useSelector.mockImplementation(callback => callback({
-      techs: ['NodeJS', 'React']
-    }))
+    useSelector.mockImplementation(callback =>
+      callback({
+        techs: ['NodeJS', 'React']
+      })
+    )
 
     const { getByTestId, getByText } = render(<TechList />)
 
     expect(getByTestId('tech-list')).toContainElement(getByText('NodeJS'))
     expect(getByTestId('tech-list')).toContainElement(getByText('React'))
+  })
+
+  it('should be able to add new tech', () => {
+    const { getByTestId, getByLabelText } = render(<TechList />)
+    const dispatch = jest.fn()
+
+    useDispatch.mockReturnValue(dispatch)
+
+    fireEvent.change(getByLabelText('Tech'), { target: { value: 'NodeJS' } })
+    fireEvent.submit(getByTestId('tech-form'))
+
+    expect(dispatch).toHaveBeenCalledWith(addTech('NodeJS'))
   })
 })
