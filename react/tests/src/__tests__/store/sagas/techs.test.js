722fcc50eb68642f0415621a7235cd2a97381cc8
Ricardo Mendes <ricardomlealjr@gmail.com>
2019-09-23 09:04:34 -0300

    Mocking the api response to the test

--- react/tests/src/__tests__/store/sagas/techs.test.js
+++ react/tests/src/__tests__/store/sagas/techs.test.js
@@ -1,13 +1,31 @@
 import { runSaga } from 'redux-saga'
-import { getTechsSuccess } from '../../../store/modules/techs/action'
+import MockAdapter from 'axios-mock-adapter'
+import api from '../../../services/api'
+
+import {
+  getTechsSuccess,
+  getTechsFailure
+} from '../../../store/modules/techs/action'
 import { getTechs } from '../../../store/modules/techs/sagas'
+
+const apiMock = new MockAdapter(api)
 
 describe('techs saga', () => {
   it('should be able to fetch techs', async () => {
     const dispatch = jest.fn()
 
+    apiMock.onGet('techs').reply(200, ['NodeJS'])
     await runSaga({ dispatch }, getTechs).toPromise()
 
     expect(dispatch).toHaveBeenCalledWith(getTechsSuccess(['NodeJS']))
+  })
+
+  it('should fail when api returns error', async () => {
+    const dispatch = jest.fn()
+
+    apiMock.onGet('techs').reply(500)
+    await runSaga({ dispatch }, getTechs).toPromise()
+
+    expect(dispatch).toHaveBeenCalledWith(getTechsFailure())
   })
 })
